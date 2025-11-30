a74c6aab60a38335456bca9f70b7ddb6c2602b17
Bivek Heka <bivek@example.com>
2025-11-30 19:15:36 +0545

    draw_spirograph vanni func add garya

--- spirograph.py
+++ spirograph.py
@@ -11,12 +11,15 @@ def random_color():
     color=(r,g,b)
     return color 
 
-for _ in range(100):
-    kalam.speed("fastest")
-    kalam.color(random_color())
-    kalam.circle(100)
-    current_heading=kalam.heading()
-    kalam.setheading(current_heading + 10)
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        kalam.speed("fastest")
+        kalam.color(random_color())
+        kalam.circle(100)
+        current_heading=kalam.heading()
+        kalam.setheading(current_heading + size_of_gap)
+
+draw_spirograph(5)
 
 screen = t.Screen()
 screen.exitonclick()
